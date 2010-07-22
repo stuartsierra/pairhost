a3fb05ac54b9123bc95635f260827325eead25da
Stuart Sierra <mail@stuartsierra.com>
2010-07-21 20:04:23 -0400

    Add certificate and private key files to env

--- bin/env.sh
+++ bin/env.sh
@@ -4,12 +4,16 @@
 # current directory must be the parent of the directory containing
 # this file.
 
-EC2_API_BIN_DIR=`ls -d ec2-api-tools-*`
-EC2_AMI_BIN_DIR=`ls -d ec2-ami-tools-*`
+EC2_API_DIR=`ls -d ec2-api-tools-*`
+EC2_AMI_DIR=`ls -d ec2-ami-tools-*`
+EC2_PRIVATE_KEY=`ls pk-*.pem`
+EC2_CERT=`ls cert-*.pem`
+ID_RSA_PRIVATE_KEY=`ls id_rsa-*`
+PRIVATE_KEY_NAME=`echo $ID_RSA_PRIVATE_KEY | perl -pne 's/id_rsa-//go'`
 
 ERROR=0
 
-if [ -z "$EC2_API_BIN_DIR" ]; then
+if [ -z "$EC2_API_DIR" ]; then
     echo "============================================================"
     echo "ERROR: Missing ec2-api-tools-* directory."
     echo "    Download ec2-api-tools.zip from"
@@ -18,7 +22,7 @@ if [ -z "$EC2_API_BIN_DIR" ]; then
     ERROR=1
 fi
 
-if [ -z "$EC2_AMI_BIN_DIR" ]; then
+if [ -z "$EC2_AMI_DIR" ]; then
     echo "============================================================"
     echo "ERROR: Missing ec2-ami-tools-* directory."
     echo "    Download ec2-ami-tools.zip from"
@@ -27,4 +31,33 @@ if [ -z "$EC2_AMI_BIN_DIR" ]; then
     ERROR=1
 fi
 
+if [ -z "$EC2_PRIVATE_KEY" ]; then
+    echo "============================================================"
+    echo "ERROR: Missing EC2 private key file."
+    echo "    Get the file with a name like pk-HKZYKTAIG2ECMXYIBH3HXV4ZBZQ55CLO.pem"
+    echo "    and copy it to this directory."
+    ERROR=1
+fi
+
+if [ -z "$EC2_CERT" ]; then
+    echo "============================================================"
+    echo "ERROR: Missing EC2 certificate file."
+    echo "    Get the file with a name like cert-HKZYKTAIG2ECMXYIBH3HXV4ZBZQ55CLO.pem"
+    echo "    and copy it to this directory."
+    ERROR=1
+fi
+
+if [ -z "$ID_RSA_PRIVATE_KEY" ]; then
+    echo "============================================================"
+    echo "ERROR: Missing SSH private key file"
+    echo "    Get the file with a name like id_rsa-*"
+    echo "    and copy it to this directory."
+    echo "    The file name, minus the id_rsa- part, should match"
+    echo "    the name of your generated key pair."
+    ERROR=1
+fi
+
 if [ $ERROR == 1 ]; then exit -1; fi
+
+export PATH=$EC2_API_DIR/bin:$EC2_AMI_DIR/bin:$PATH
+export EC2_HOME=$EC2_API_DIR
